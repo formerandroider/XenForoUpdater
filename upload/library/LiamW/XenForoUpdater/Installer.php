06881837893a0318ea3b41a401ba48122218e0c2
Liam Williams <l.s.w5559@gmail.com>
2017-11-08 01:42:44 +0000

    Version 1.1.5.1 - Remove File Hash Checks

--- upload/library/LiamW/XenForoUpdater/Installer.php
+++ upload/library/LiamW/XenForoUpdater/Installer.php
@@ -11,22 +11,6 @@ class LiamW_XenForoUpdater_Installer
 			return false;
 		}
 
-		$hashErrors = XenForo_Helper_Hash::compareHashes(LiamW_XenForoUpdater_FileSums::getHashes());
-
-		if ($hashErrors)
-		{
-			$error = "The following files could not be found or contain unexpected contents: <ul>";
-
-			foreach ($hashErrors AS $file => $fileError)
-			{
-				$error .= "<li>$file - " . ($fileError == 'mismatch' ? 'File contains unexpected contents' : 'File not found') . "</li>";
-			}
-
-			$error .= "</ul>";
-
-			return false;
-		}
-
 		if (!LiamW_XenForoUpdater_Helper::zipInstalled())
 		{
 			$error = 'The ZIP extension is required for this add-on to work. Please ask your host to recompile PHP with the <a href="http://php.net/manual/en/book.zip.php" target="_blank"><i>zip</i></a> extension.';
